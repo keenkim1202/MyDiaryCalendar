124d76ede3004e9dbe887261008d49bb42db32c3
KEEN <homurahomu@naver.com>
2021-11-03 10:23:47 +0900

    도큐먼트에 이미지 정보 저장 함수 작성

--- MyDiaryCalendar/Sources/ViewController/AddViewController.swift
+++ MyDiaryCalendar/Sources/ViewController/AddViewController.swift
@@ -32,11 +32,48 @@ class AddViewController: UIViewController {
   }
   
   @objc func onSave() {
-    let task = UserDiary(diaryTitle: titleTextField.text!, content: contentTextField.text!, writtenDate: Date(), regDate: Date())
+    let task = UserDiary(
+      diaryTitle: titleTextField.text!, content: contentTextField.text!,
+      writtenDate: Date(), regDate: Date()
+    )
+    
     try! localRealm.write {
       localRealm.add(task)
     }
-    
     self.dismiss(animated: true, completion: nil)
+  }
+  
+  func saveImageToDocumentDirectory(imageName: String, image: UIImage) {
+    // 1. 이미지 저장할 경로 설정: 도큐먼트 폴더, FileManager
+    /// ex. Desktop/keen/ios/folder
+    guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+    
+    // 2. 이미지 파일 이름 & 최종 경로 설정
+    /// ex. Desktop/keen/ios/folder/222.jpg
+    let imageURL = documentDirectory.appendingPathComponent(imageName)
+    
+    // 3. 이미지 압축
+//    guard let data = image.pngData() else { return }
+    guard let data = image.jpegData(compressionQuality: 0.2) else { return }
+    
+    // 4. 이미지 저장: 동일한 경로에 이미지를 저장하게 될 경우, 덮어쓰기
+    /// 4-1. 이미지 경로 여부 확인
+    if FileManager.default.fileExists(atPath: imageURL.path) {
+      // 4-2. 기존 경로에 있는 이미지 삭제
+      do {
+        try FileManager.default.removeItem(at: imageURL)
+        print("REMOVE SUCCESS")
+      } catch {
+        print("REMOVE FAILED")
+      }
+    }
+    
+    // 5. 이미지를 도큐먼트에 저장
+    do {
+      try data.write(to: imageURL)
+    } catch {
+      print("WRITE FAILED")
+    }
+    
   }
 }
