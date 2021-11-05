7cfe6e284a1725a6a4cc598667c414fc800528f6
KEEN <homurahomu@naver.com>
2021-11-05 11:52:26 +0900

    Realm 관련 코드 분리

--- MyDiaryCalendar/Sources/Realm/RealmQuery.swift
+++ MyDiaryCalendar/Sources/Realm/RealmQuery.swift
@@ -5,4 +5,22 @@
 //  Created by KEEN on 2021/11/05.
 //
 
-import Foundation
+import UIKit
+import RealmSwift
+
+// TODO: Realm 과 과련된 부분 분리해서 extension에 작성하기
+
+extension UIViewController {
+  // MARK: - Method
+  func searchQuryFromUserDiary(q: String) -> Results<UserDiary> {  // 원하는 일기 가져오기
+    let localRealm = try! Realm()
+    let search = localRealm.objects(UserDiary.self).filter("diaryTitle CONTAINTS[c] '\(q)' OR content CONTAINTS[c] '\(q)'")
+    
+    return search
+  }
+  
+  func getAllDiaryCountFromUserDiary() -> Int {
+    let localRealm = try! Realm()
+    return localRealm.objects(UserDiary.self).count
+  }
+}
