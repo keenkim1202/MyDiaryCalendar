d4563e1bc09c3081c9066f7fcaaae4472ed941ec
KEEN <homurahomu@naver.com>
2021-11-05 10:46:20 +0900

    FSCalander 설치 및 UI 구성

--- MyDiaryCalendar/Sources/ViewController/CalendarViewController.swift
+++ MyDiaryCalendar/Sources/ViewController/CalendarViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import FSCalendar
 
 class CalendarViewController: UIViewController {
 
-  // MARK: View Life-Cycle
+  // MARK: - UI
+  @IBOutlet weak var calendarView: FSCalendar!
+  
+  // MARK: - View Life-Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    calendarView.delegate = self
+    calendarView.dataSource = self
     
     title = "달력"
   }
 
+}
+
+// MARK: - Extension
+// MARK: - FSCalendarDelegate
+extension CalendarViewController: FSCalendarDelegate {
+  
+}
+
+// MARK: - FSCalendarDataSource
+extension CalendarViewController: FSCalendarDataSource {
+  func calendar(_ calendar: FSCalendar, titleFor date: Date) -> String? {
+    return "title"
+  }
+  
+  func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
+    return "subTitle"
+  }
+  
+  func calendar(_ calendar: FSCalendar, imageFor date: Date) -> UIImage? {
+    return UIImage(systemName: "star")
+  }
+  
+  func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+    return 2
+  }
 }
